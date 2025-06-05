f4795015f9da6207f08eeebb5475b3bc2f866179
Ifedapo Rasaq <arasaq040@gmail.com>
2025-06-05 12:10:47 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,5 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1,num2,operation):
     operation = operation.lower()
-    
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
